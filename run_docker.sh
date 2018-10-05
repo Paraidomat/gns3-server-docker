d5c2362e710b19e949aeba52031fe69222b69cff
Paraidomat <S-k-y.F-i-r-e@gmx.de>
2018-10-05 10:51:53 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,6 @@
+# Documentation:
+# https://docs.docker.com/engine/reference/commandline/run/#options
+
 touch ~/gns3log
 
 docker run --name=gns3 -d -v ~/gns3log:/var/log/gns3server -p 3080:3080 gns3server:test
